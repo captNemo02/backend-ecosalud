94cae0fc1e0753943d7c20068659760e979232fe
GeldXand <geraldaraujovillanueva@hotmail.com>
2026-06-19 19:00:05 -0500

    Update service.py

--- app/modules/pacientes/service.py
+++ app/modules/pacientes/service.py
@@ -132,15 +132,26 @@ def get_ordenes_medicas_by_paciente(db: Session, paciente_id: int):
 DOCTORES_SERVICE_URL = "https://serviciodoctor.onrender.com"
 CLINICA_SERVICE_URL = "https://api-clinica-soa.onrender.com/"
 
-async def get_recetas_by_paciente_remoto(paciente_id: int):
+async def get_recetas_by_paciente_remoto(paciente_id: int, db: Session):
     """
     Se conecta vía HTTP con el microservicio de doctores para obtener 
-    las recetas reales emitidas a este paciente.
+    las recetas reales emitidas a este paciente usando su DNI.
     """
+    # 1. Buscamos el paciente en TU base de datos usando el id que viene de React
+    paciente = db.query(models.Paciente).filter(models.Paciente.id == paciente_id).first()
+    
+    if not paciente:
+        raise HTTPException(status_code=404, detail="Paciente no encontrado en el sistema.")
+
+    # 2. Extraemos su número de documento (DNI)
+    dni_paciente = paciente.numero_documento
+
     async with httpx.AsyncClient() as client:
         try:
             url_final = f"{DOCTORES_SERVICE_URL}/doctor/recetas-paciente"
-            response = await client.get(url_final, params={"paciente_id": paciente_id})
+            
+            # Cambiamos el parámetro "paciente_id" por "dni" (o el nombre exacto que use tu compañero)
+            response = await client.get(url_final, params={"dni": dni_paciente})
             
             if response.status_code != 200:
                 raise HTTPException(
@@ -153,7 +164,7 @@ async def get_recetas_by_paciente_remoto(paciente_id: int):
         except httpx.RequestError:
             raise HTTPException(
                 status_code=503, 
-                detail="No se pudo establecer comunicación con el microservicio de doctores (Servicio Tempormente No Disponible)."
+                detail="No se pudo establecer comunicación con el microservicio de doctores (Servicio Temporalmente No Disponible)."
             )
 
 async def get_check_recordatorio_cita(paciente_id: int, db: Session = None):
